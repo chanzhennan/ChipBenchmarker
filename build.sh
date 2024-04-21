3c2365e48cdaeca4337e5a8d890c8f9e54cc7517
chenyonghua <chenyonghua@infini-ai.com>
2024-04-21 03:31:24 +0000

    [fix]: build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
+# Remove the previous 'build' directory and create a new one
 rm -rf build
 mkdir build && cd build
-cmake -DBUILD_WITH_CUDA=ON -DARCH=sm_80 ..
+
+## CUDA Platform ###########
+# cmake -DBUILD_WITH_CUDA=ON -DARCH=sm_80 ..
+
+## HIP Platform ############
+# cmake -DBUILD_WITH_MI210=ON ..
+
+## MX Platform ############
+#
+
+cmake -DBUILD_WITH_MI210=ON ..
 make -j
